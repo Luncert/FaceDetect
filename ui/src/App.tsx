78ef4c21e0972f1d18e5e9042ac196ee21d5a8d1
Luncert <2725115515@qq.com>
2019-10-22 23:06:09 +0800

    .

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Layout, Typography } from 'antd'
+import { Layout, Typography, Button } from 'antd'
 import './App.css'
 
 interface State {
@@ -7,10 +7,31 @@ interface State {
 
 export default class App extends React.Component<any, State> {
   
+  private canvasContext: CanvasRenderingContext2D | null = null
   constructor(props: any) {
-      super(props);
+      super(props)
       this.state = {
       }
+  }
+
+  componentDidMount() {
+    let canvas = (this.refs.canvas as HTMLCanvasElement)
+    this.canvasContext = canvas.getContext('2d')
+    let video = (this.refs.video as HTMLVideoElement)
+    const errHandle = 
+    navigator.getUserMedia({'video': true},
+      (stream) => {
+        video.srcObject = stream
+        video.play()
+      }, 
+      (err: MediaStreamError) => console.error(err))
+  }
+
+  drawImage() {
+    if (this.canvasContext != null) {
+      let video = (this.refs.video as HTMLVideoElement)
+      this.canvasContext.drawImage(video, 0, 0, 500, 300);
+    }
   }
 
   render() {
@@ -22,6 +43,9 @@ export default class App extends React.Component<any, State> {
           <Typography.Title style={{color: 'white', textShadow: '1px 1px 3px black'}}>Face Detect</Typography.Title>
         </Layout.Header>
         <Layout.Content style={{width: '60%', height: '100%', margin: '0px auto'}}>
+          <video ref='video' width={500} height={300} style={{border: '1px black solid'}}></video>
+          <canvas ref='canvas' width={500} height={300} style={{border: '1px black solid'}}></canvas>
+          <Button type="primary">Capture</Button>
         </Layout.Content>
       </Layout>
     )
